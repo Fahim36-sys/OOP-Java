9b4274aef944c80f3934b549617e29391592a0de
Md. Fahim Hasnat <fahimhasnat32@outlook.com>
2026-06-11 20:58:58 +0600

    New program added

---
+++ CalculatorApp/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+        int sum = calculator.add(a, b);
+        int difference = calculator.subtract(a, b);
+        int product = calculator.multiply(a, b);
+        double quotient = calculator.divide(a, b);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
