a107e82ba73d24970b20a9973630e0d351b75ae2
Md. Fahim Hasnat <fahimhasnat32@outlook.com>
2026-06-12 21:57:58 +0600

    gg

--- CalculatorApp/Main.java
+++ CalculatorApp/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the first number: ");
         int a = scanner.nextInt();
