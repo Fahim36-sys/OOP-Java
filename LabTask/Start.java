bc6a1ea723eb1c482e808939635080fe352af470
Md. Fahim Hasnat <fahimhasnat32@outlook.com>
2026-06-17 14:25:04 +0600

    change added

--- LabTask/Start.java
+++ LabTask/Start.java
@@ -3,9 +3,9 @@ public class Start{
         Student s1=new Student();
         Student s2=new Student("Shaoda","26-64009-1",3.15f);
         Student s3=new Student("Orpon","26-64573-1",3.00f);
-        s1.name="Fahim";
-        s1.id="26-64080-1";
-        s1.cgpa=3.5f;
+        s1.setName("Fahim");
+        s1.setId("26-64080-1");
+        s1.setCgpa(3.5f);
         s1.show();
         s2.show();
         s3.show();
